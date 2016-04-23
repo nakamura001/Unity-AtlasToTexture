94271f2acdb0f4b81c7167c2adbdff9b8781ea30
nakamura001 <polluxtk@gmail.com>
2016-04-23 18:52:32 +0900

    Initial commit.

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+	public Image img;
+	public RawImage raw;
+
+	public void OnTest() {
+		raw.texture = img.sprite.texture;
+		Rect rc = img.sprite.textureRect;
+		rc.x = rc.x / img.sprite.texture.width;
+		rc.width = rc.width / img.sprite.texture.width;
+		rc.y = rc.y / img.sprite.texture.height;
+		rc.height = rc.height / img.sprite.texture.height;
+		raw.uvRect = rc;
+		//img.sprite = null;
+	}
+}
